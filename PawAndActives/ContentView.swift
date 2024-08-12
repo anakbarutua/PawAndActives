1a0e1f7972aea48d77580b2e7a0da9d37307f0f8
Anjar Harimurti <anjarharimurti.ah@gmail.com>
2024-08-13 00:12:20 +0700

    Initial Commit

---
+++ PawAndActives/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PawAndActives
+//
+//  Created by Anjar Harimurti on 12/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Text")
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawAndActives/PawAndActivesApp.swift
@@ -0,0 +1,19 @@
+//
+//  PawAndActivesApp.swift
+//  PawAndActives
+//
+//  Created by Anjar Harimurti on 12/08/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PawAndActivesApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
