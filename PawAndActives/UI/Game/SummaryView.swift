37186433a1b6c6c150046ed65840d94f16fb1399
Anjar Harimurti <anjarharimurti.ah@gmail.com>
2024-08-13 03:24:34 +0700

    feat: [ABT-4] Initialize Routing

--- PawAndActives/UI/Game/SummaryView.swift
+++ PawAndActives/UI/Game/SummaryView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SummaryView: View {
+    @EnvironmentObject var navigationManager: NavigationManager
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
