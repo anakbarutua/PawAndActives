2f18ef9d7e5805ccd1d0f4ad8b4431fe8aecf9b5
Anjar Harimurti <anjarharimurti.ah@gmail.com>
2024-08-14 02:58:48 +0700

    feat: [ABT-7] Create Camera Implementation using AVFoundation

--- PawAndActives/Manager/Camera/CameraManager.swift
+++ PawAndActives/Manager/Camera/CameraManager.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+import AVFoundation
+import Vision
+
+class CameraManager: NSObject, CameraService {
+    
+    private var captureSession: AVCaptureSession?
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    private var sessionQueue = DispatchQueue(label: "session queue")
+    
+
+    override init() {
+        super.init()
+        setupSession()
+    }
+    
+    private func setupSession() {
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = .high
+        
+        guard let captureSession = captureSession else { return }
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            print("Error: \(error)")
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            print("Could not add video input to session")
+            return
+        }
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: sessionQueue)
+        if (captureSession.canAddOutput(videoOutput)) {
+            captureSession.addOutput(videoOutput)
+        } else {
+            print("Could not add video output to session")
+            return
+        }
+        
+        sessionQueue.async {
+            self.captureSession?.startRunning()
+        }
+    }
+    
+    func startSession() {
+        sessionQueue.async {
+            self.captureSession?.startRunning()
+        }
+    }
+    
+    func stopSession() {
+        sessionQueue.async {
+            self.captureSession?.stopRunning()
+        }
+    }
+    
+    func getPreviewLayer() -> AVCaptureVideoPreviewLayer? {
+        if previewLayer == nil, let captureSession = captureSession {
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer?.videoGravity = .resizeAspectFill
+        }
+        return previewLayer
+    }
+}
+
+extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+    }
+}
